ad2877d0c4c2b7898f69c86f51bdfeb7c8167641
roymamon <mamon8521@gmail.com>
2025-05-26 17:36:07 +0000

    implemented part 2

--- Part_2/molecule_supplier.c
+++ Part_2/molecule_supplier.c
@@ -8,7 +8,6 @@
 #include <sys/select.h>
 #include <arpa/inet.h>
 
-#define PORT 5555
 #define BUF_SIZE 1024
 #define MAX_ATOMS 1000000000000000000ULL
 
@@ -22,21 +21,21 @@ void update_inventory(int client_fd, Inventory* inv, const char* atom, unsigned 
     char msg[BUF_SIZE];
 
     if (strcmp(atom, "CARBON") == 0) {
-        if (inv->carbon + amount > MAX_ATOMS) {
+        if (amount > MAX_ATOMS - inv->carbon) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
             return;
         }
         inv->carbon += amount;
     } else if (strcmp(atom, "HYDROGEN") == 0) {
-        if (inv->hydrogen + amount > MAX_ATOMS) {
+        if (amount > MAX_ATOMS - inv->hydrogen) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
             return;
         }
         inv->hydrogen += amount;
     } else if (strcmp(atom, "OXYGEN") == 0) {
-        if (inv->oxygen + amount > MAX_ATOMS) {
+        if (amount > MAX_ATOMS - inv->oxygen) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
             return;
@@ -81,35 +80,49 @@ void do_delivery(const char* molecule, unsigned long long count, Inventory* inv)
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <tcp_port> <udp_port>\n", argv[0]);
+        exit(1);
+    }
+
+    int tcp_port = atoi(argv[1]);
+    int udp_port = atoi(argv[2]);
+
     int tcp_fd, udp_fd, fdmax;
-    struct sockaddr_in address, udp_addr;
+    struct sockaddr_in tcp_addr, udp_addr, client_addr;
     fd_set master_set, read_fds;
     Inventory inv = {0};
     char buffer[BUF_SIZE];
 
-    // TCP setup
+    // TCP socket setup
     tcp_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (tcp_fd < 0) { perror("tcp socket"); exit(1); }
 
     int opt = 1;
     setsockopt(tcp_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    address.sin_family = AF_INET;
-    address.sin_port = htons(PORT);
-    address.sin_addr.s_addr = INADDR_ANY;
+    memset(&tcp_addr, 0, sizeof(tcp_addr));
+    tcp_addr.sin_family = AF_INET;
+    tcp_addr.sin_port = htons(tcp_port);
+    tcp_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(tcp_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    if (bind(tcp_fd, (struct sockaddr*)&tcp_addr, sizeof(tcp_addr)) < 0) {
         perror("tcp bind"); exit(1);
     }
 
     listen(tcp_fd, SOMAXCONN);
 
-    // UDP setup
+    // UDP socket setup
     udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (udp_fd < 0) { perror("udp socket"); exit(1); }
 
-    if (bind(udp_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    memset(&udp_addr, 0, sizeof(udp_addr));
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_port = htons(udp_port);
+    udp_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(udp_fd, (struct sockaddr*)&udp_addr, sizeof(udp_addr)) < 0) {
         perror("udp bind"); exit(1);
     }
 
@@ -118,7 +131,7 @@ int main() {
     FD_SET(udp_fd, &master_set);
     fdmax = tcp_fd > udp_fd ? tcp_fd : udp_fd;
 
-    printf("molecule_supplier running on port %d\n", PORT);
+    printf("molecule_supplier running (TCP: %d, UDP: %d)\n", tcp_port, udp_port);
 
     while (1) {
         read_fds = master_set;
@@ -129,19 +142,19 @@ int main() {
         for (int i = 0; i <= fdmax; ++i) {
             if (FD_ISSET(i, &read_fds)) {
                 if (i == tcp_fd) {
-                    // New TCP connection
-                    socklen_t addrlen = sizeof(address);
-                    int client_fd = accept(tcp_fd, (struct sockaddr*)&address, &addrlen);
+                    // Accept TCP connection
+                    socklen_t addrlen = sizeof(client_addr);
+                    int client_fd = accept(tcp_fd, (struct sockaddr*)&client_addr, &addrlen);
                     if (client_fd >= 0) {
                         FD_SET(client_fd, &master_set);
                         if (client_fd > fdmax) fdmax = client_fd;
                         printf("atom_supplier connected\n");
                     }
                 } else if (i == udp_fd) {
-                    // UDP request
-                    socklen_t addrlen = sizeof(udp_addr);
+                    // Handle UDP request
+                    socklen_t addrlen = sizeof(client_addr);
                     ssize_t len = recvfrom(udp_fd, buffer, BUF_SIZE - 1, 0,
-                                           (struct sockaddr*)&udp_addr, &addrlen);
+                                           (struct sockaddr*)&client_addr, &addrlen);
                     if (len > 0) {
                         buffer[len] = 0;
                         char cmd[16], mol[32];
@@ -150,18 +163,18 @@ int main() {
                             if (can_deliver(mol, n, &inv)) {
                                 do_delivery(mol, n, &inv);
                                 sendto(udp_fd, "DELIVERED\n", 10, 0,
-                                       (struct sockaddr*)&udp_addr, addrlen);
+                                       (struct sockaddr*)&client_addr, addrlen);
                             } else {
                                 sendto(udp_fd, "FAILURE\n", 8, 0,
-                                       (struct sockaddr*)&udp_addr, addrlen);
+                                       (struct sockaddr*)&client_addr, addrlen);
                             }
                         } else {
                             sendto(udp_fd, "INVALID REQUEST\n", 17, 0,
-                                   (struct sockaddr*)&udp_addr, addrlen);
+                                   (struct sockaddr*)&client_addr, addrlen);
                         }
                     }
                 } else {
-                    // TCP command from atom_supplier
+                    // Handle TCP atom_supplier command
                     ssize_t len = recv(i, buffer, BUF_SIZE - 1, 0);
                     if (len <= 0) {
                         close(i);
