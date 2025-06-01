93da5af7d923aa313b743809883951fadce8c6aa
roymamon <mamon8521@gmail.com>
2025-06-01 14:10:22 +0000

    updated part 6

--- Part_6/drinks_bar.c
+++ Part_6/drinks_bar.c
@@ -10,6 +10,10 @@
 #include <getopt.h>
 #include <errno.h>
 #include <sys/un.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
 #define MAX_ATOMS 1000000000000000000ULL
@@ -20,31 +24,43 @@ typedef struct {
     unsigned long long oxygen;
 } Inventory;
 
+Inventory *inv = NULL;
+int inventory_fd = -1;
+
 unsigned long long min(unsigned long long a, unsigned long long b) {
     return a < b ? a : b;
 }
 
-void update_inventory(int client_fd, Inventory* inv, const char* atom, unsigned long long amount) {
+void lock_inventory() {
+    flock(inventory_fd, LOCK_EX);
+}
+
+void unlock_inventory() {
+    flock(inventory_fd, LOCK_UN);
+}
+
+void update_inventory(int client_fd, const char* atom, unsigned long long amount) {
     char msg[BUF_SIZE];
+    lock_inventory();
     if (strcmp(atom, "CARBON") == 0) {
         if (amount > MAX_ATOMS - inv->carbon) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
-            return;
+            unlock_inventory(); return;
         }
         inv->carbon += amount;
     } else if (strcmp(atom, "HYDROGEN") == 0) {
         if (amount > MAX_ATOMS - inv->hydrogen) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
-            return;
+            unlock_inventory(); return;
         }
         inv->hydrogen += amount;
     } else if (strcmp(atom, "OXYGEN") == 0) {
         if (amount > MAX_ATOMS - inv->oxygen) {
             snprintf(msg, sizeof(msg), "error: exceeding storage limit.\n");
             send(client_fd, msg, strlen(msg), 0);
-            return;
+            unlock_inventory(); return;
         }
         inv->oxygen += amount;
     }
@@ -52,21 +68,26 @@ void update_inventory(int client_fd, Inventory* inv, const char* atom, unsigned 
              "inventory: CARBON=%llu, HYDROGEN=%llu, OXYGEN=%llu\n",
              inv->carbon, inv->hydrogen, inv->oxygen);
     send(client_fd, msg, strlen(msg), 0);
+    unlock_inventory();
 }
 
-int can_deliver(const char* molecule, unsigned long long count, Inventory* inv) {
+int can_deliver(const char* molecule, unsigned long long count) {
+    lock_inventory();
+    int ok = 0;
     if (strcmp(molecule, "WATER") == 0)
-        return inv->hydrogen >= 2 * count && inv->oxygen >= count;
-    if (strcmp(molecule, "CARBON_DIOXIDE") == 0)
-        return inv->carbon >= count && inv->oxygen >= 2 * count;
-    if (strcmp(molecule, "GLUCOSE") == 0)
-        return inv->carbon >= 6 * count && inv->hydrogen >= 12 * count && inv->oxygen >= 6 * count;
-    if (strcmp(molecule, "ALCOHOL") == 0)
-        return inv->carbon >= 2 * count && inv->hydrogen >= 6 * count && inv->oxygen >= count;
-    return 0;
+        ok = inv->hydrogen >= 2 * count && inv->oxygen >= count;
+    else if (strcmp(molecule, "CARBON_DIOXIDE") == 0)
+        ok = inv->carbon >= count && inv->oxygen >= 2 * count;
+    else if (strcmp(molecule, "GLUCOSE") == 0)
+        ok = inv->carbon >= 6 * count && inv->hydrogen >= 12 * count && inv->oxygen >= 6 * count;
+    else if (strcmp(molecule, "ALCOHOL") == 0)
+        ok = inv->carbon >= 2 * count && inv->hydrogen >= 6 * count && inv->oxygen >= count;
+    unlock_inventory();
+    return ok;
 }
 
-void do_delivery(const char* molecule, unsigned long long count, Inventory* inv) {
+void do_delivery(const char* molecule, unsigned long long count) {
+    lock_inventory();
     if (strcmp(molecule, "WATER") == 0) {
         inv->hydrogen -= 2 * count;
         inv->oxygen -= count;
@@ -82,13 +103,15 @@ void do_delivery(const char* molecule, unsigned long long count, Inventory* inv)
         inv->hydrogen -= 6 * count;
         inv->oxygen -= count;
     }
+    unlock_inventory();
 }
 
-void handle_keyboard(Inventory* inv, const char* line) {
+void handle_keyboard(const char* line) {
     if (strncmp(line, "GEN ", 4) != 0) return;
     line += 4;
     unsigned long long result = 0;
 
+    lock_inventory();
     if (strcmp(line, "SOFT DRINK") == 0) {
         result = inv->carbon / 7;
         result = min(result, inv->hydrogen / 14);
@@ -102,14 +125,25 @@ void handle_keyboard(Inventory* inv, const char* line) {
         result = min(result, inv->hydrogen / 8);
         result = min(result, inv->oxygen / 4);
     }
+    unlock_inventory();
 
     printf("can produce %llu of %s\n", result, line);
 }
 
+void setup_inventory_file(const char* path) {
+    int exists = access(path, F_OK) == 0;
+    inventory_fd = open(path, O_RDWR | O_CREAT, 0666);
+    if (inventory_fd < 0) { perror("open"); exit(1); }
+    if (!exists) ftruncate(inventory_fd, sizeof(Inventory));
+    inv = mmap(NULL, sizeof(Inventory), PROT_READ | PROT_WRITE, MAP_SHARED, inventory_fd, 0);
+    if (inv == MAP_FAILED) { perror("mmap"); exit(1); }
+    if (!exists) memset(inv, 0, sizeof(Inventory));
+}
+
 int main(int argc, char* argv[]) {
     int tcp_port = -1, udp_port = -1, timeout = -1;
-    char *stream_path = NULL, *dgram_path = NULL;
-    Inventory inv = {0};
+    char *stream_path = NULL, *dgram_path = NULL, *save_file = NULL;
+    Inventory static_inv = {0};
 
     struct option long_opts[] = {
         {"oxygen", required_argument, NULL, 'o'},
@@ -120,23 +154,22 @@ int main(int argc, char* argv[]) {
         {"udp-port", required_argument, NULL, 'U'},
         {"stream-path", required_argument, NULL, 's'},
         {"datagram-path", required_argument, NULL, 'd'},
+        {"save-file", required_argument, NULL, 'f'},
         {NULL, 0, NULL, 0}
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "o:c:h:t:T:U:s:d:", long_opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "o:c:h:t:T:U:s:d:f:", long_opts, NULL)) != -1) {
         switch (opt) {
-            case 'o': inv.oxygen = strtoull(optarg, NULL, 10); break;
-            case 'c': inv.carbon = strtoull(optarg, NULL, 10); break;
-            case 'h': inv.hydrogen = strtoull(optarg, NULL, 10); break;
+            case 'o': static_inv.oxygen = strtoull(optarg, NULL, 10); break;
+            case 'c': static_inv.carbon = strtoull(optarg, NULL, 10); break;
+            case 'h': static_inv.hydrogen = strtoull(optarg, NULL, 10); break;
             case 't': timeout = atoi(optarg); break;
             case 'T': tcp_port = atoi(optarg); break;
             case 'U': udp_port = atoi(optarg); break;
             case 's': stream_path = optarg; break;
             case 'd': dgram_path = optarg; break;
-            default:
-                fprintf(stderr, "Usage: %s -T <tcp_port> -U <udp_port> [--oxygen N] [--carbon N] [--hydrogen N] [--timeout N] [--stream-path PATH] [--datagram-path PATH]\n", argv[0]);
-                exit(1);
+            case 'f': save_file = optarg; break;
         }
     }
 
@@ -145,14 +178,15 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    if (save_file) {
+        setup_inventory_file(save_file);
+    } else {
+        inv = &static_inv;
+    }
+
     int tcp_fd = socket(AF_INET, SOCK_STREAM, 0);
     int udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
     int stream_fd = -1, dgram_fd = -1;
-
-    if (tcp_fd < 0 || udp_fd < 0) { perror("socket"); exit(1); }
-
-    int optval = 1;
-    setsockopt(tcp_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
     struct sockaddr_in tcp_addr = {.sin_family = AF_INET, .sin_port = htons(tcp_port), .sin_addr.s_addr = INADDR_ANY};
     struct sockaddr_in udp_addr = {.sin_family = AF_INET, .sin_port = htons(udp_port), .sin_addr.s_addr = INADDR_ANY};
@@ -191,7 +225,6 @@ int main(int argc, char* argv[]) {
     if (dgram_fd > fdmax) fdmax = dgram_fd;
 
     printf("drinks_bar running\n");
-
     char buffer[BUF_SIZE];
 
     while (1) {
@@ -218,7 +251,6 @@ int main(int argc, char* argv[]) {
                 if (client_fd >= 0) {
                     FD_SET(client_fd, &master_set);
                     if (client_fd > fdmax) fdmax = client_fd;
-                    printf("client connected\n");
                 }
             } else if (i == udp_fd || (dgram_fd != -1 && i == dgram_fd)) {
                 struct sockaddr_storage client;
@@ -229,8 +261,8 @@ int main(int argc, char* argv[]) {
                     char cmd[16], mol[32];
                     unsigned long long count;
                     if (sscanf(buffer, "%15s %31s %llu", cmd, mol, &count) == 3 && strcmp(cmd, "DELIVER") == 0) {
-                        if (can_deliver(mol, count, &inv)) {
-                            do_delivery(mol, count, &inv);
+                        if (can_deliver(mol, count)) {
+                            do_delivery(mol, count);
                             sendto(i, "DELIVERED\n", 10, 0, (struct sockaddr*)&client, len);
                         } else {
                             sendto(i, "FAILURE\n", 8, 0, (struct sockaddr*)&client, len);
@@ -242,7 +274,7 @@ int main(int argc, char* argv[]) {
             } else if (i == STDIN_FILENO) {
                 if (fgets(buffer, sizeof(buffer), stdin)) {
                     buffer[strcspn(buffer, "\n")] = 0;
-                    handle_keyboard(&inv, buffer);
+                    handle_keyboard(buffer);
                 }
             } else {
                 ssize_t len = recv(i, buffer, BUF_SIZE - 1, 0);
@@ -254,7 +286,7 @@ int main(int argc, char* argv[]) {
                     char cmd[16], atom[16];
                     unsigned long long amount;
                     if (sscanf(buffer, "%15s %15s %llu", cmd, atom, &amount) == 3 && strcmp(cmd, "ADD") == 0) {
-                        update_inventory(i, &inv, atom, amount);
+                        update_inventory(i, atom, amount);
                     } else {
                         char* err = "invalid command. Use: ADD ATOM_NAME AMOUNT\n";
                         send(i, err, strlen(err), 0);
@@ -268,5 +300,9 @@ int main(int argc, char* argv[]) {
     close(udp_fd);
     if (stream_fd != -1) { close(stream_fd); unlink(stream_path); }
     if (dgram_fd != -1) { close(dgram_fd); unlink(dgram_path); }
+    if (save_file) {
+        munmap(inv, sizeof(Inventory));
+        close(inventory_fd);
+    }
     return 0;
 }
